b81c3bede2c84db5c1291f8c2490e8d2b3b139aa
sebhiJk <sebastianhinrichs@outlook.de>
2026-04-26 15:01:22 +0200

    Update philips-tv-remote.js

--- philips-tv-remote.js
+++ philips-tv-remote.js
@@ -16,7 +16,8 @@ class philipsTvRemote extends LitElement {
 
         const borderWidth = this.config.dimensions && this.config.dimensions.border_width ? this.config.dimensions.border_width : "1px";
         const scale = this.config.dimensions && this.config.dimensions.scale ? this.config.dimensions.scale : 1;
-        const remoteWidth = Math.round(scale * 260) + "px";
+        // Breite leicht erhöht, um Platz für 5 Spalten zu schaffen
+        const remoteWidth = Math.round(scale * 300) + "px";
 
         const backgroundColor = this.config.colors && this.config.colors.background ? this.config.colors.background : "var( --ha-card-background, var(--card-background-color, white) )";
         const borderColor = this.config.colors && this.config.colors.border ? this.config.colors.border: "var(--primary-text-color)";
@@ -29,30 +30,37 @@ class philipsTvRemote extends LitElement {
                   ${this.config.name
                   ? html` <span class="title"> ${this.config.name} </span> `
                   : ""}
-                  <div class="grid-container-power"  style="--remotewidth: ${remoteWidth}">
-                      <button class="btn-flat flat-high ripple" @click=${() => this._command("Source")}><ha-icon icon="mdi:import"/></button>
-                      <div></div>
-                      ${stateObj.state === 'off' ? html`
-                      <button class="btn ripple" @click=${() => this._turn_on()}><ha-icon icon="mdi:power" style="color: ${textColor};"/></button>
-                      ` : html`
-                      <button class="btn ripple" @click=${() => this._command("PowerOff")}><ha-icon icon="mdi:power" style="color: red;"/></button>
-                      `}
-                  </div> 
                 
                   <div class="grid-container-cursor">
-                  <div class="shape">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 79"><path d="m 30 15 a 10 10 0 0 1 20 0 a 15 15 0 0 0 15 15 a 10 10 0 0 1 0 20 a 15 15 0 0 0 -15 15 a 10 10 0 0 1 -20 0 a 15 15 0 0 0 -15 -15 a 10 10 0 0 1 0 -20 a 15 15 0 0 0 15 -15" fill="var(--remote-button-color)" stroke="#000000" stroke-width="0" /></svg>
-                    </div> 
-                      <button class="btn ripple item_menu" @click=${() => this._command("Options")}><ha-icon icon="mdi:format-list-numbered"/></button>
+                      <div class="shape">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 79"><path d="m 30 15 a 10 10 0 0 1 20 0 a 15 15 0 0 0 15 15 a 10 10 0 0 1 0 20 a 15 15 0 0 0 -15 15 a 10 10 0 0 1 -20 0 a 15 15 0 0 0 -15 -15 a 10 10 0 0 1 0 -20 a 15 15 0 0 0 15 -15" fill="var(--remote-button-color)" stroke="#000000" stroke-width="0" /></svg>
+                      </div> 
+                      
+                      <button class="btn ripple item_vol_up" @click=${() => this._command("VolumeUp")}><ha-icon icon="mdi:plus"/></button>
+                      <button class="btn ripple item_vol_mute" style="color:${stateObj.attributes.is_volume_muted === true ? 'red' : ''};" @click=${() => this._command("Mute")}><span class="${stateObj.attributes.is_volume_muted === true ? 'blink' : ''}"><ha-icon icon="${stateObj.attributes.is_volume_muted === true ? 'mdi:volume-off' : 'mdi:volume-mute'}"></span></button>
+                      <button class="btn ripple item_vol_down" @click=${() => this._command("VolumeDown")}><ha-icon icon="mdi:minus"/></button>
+
+                      <button class="btn ripple item_source" @click=${() => this._command("Source")}><ha-icon icon="mdi:import"/></button>
+                      <button class="btn ripple item_power" @click=${() => stateObj.state === 'off' ? this._turn_on() : this._command("PowerOff")}>
+                          <ha-icon icon="mdi:power" style="color: ${stateObj.state === 'off' ? textColor : 'red'};"/>
+                      </button>
+                      <button class="btn ripple item_options" @click=${() => this._command("Options")}><ha-icon icon="mdi:format-list-numbered"/></button>
+                      <button class="btn ripple item_back" @click=${() => this._command("Back")}><ha-icon icon="mdi:undo-variant"/></button>
+
                       <button class="btn ripple item_up" style="background-color: transparent;" @click=${() => this._command("CursorUp")}><ha-icon icon="mdi:chevron-up"/></button>
-                      <button class="btn ripple item_input" @click=${() => this._command("Back")}><ha-icon icon="mdi:undo-variant"/></button>
-                      
-                      <button class="btn ripple item_2_sx" style="background-color: transparent;" @click=${() => this._command("CursorLeft")}><ha-icon icon="mdi:chevron-left"/></button>
-                      <button class="btn bnt_ok ripple item_2_c" style="border: solid 2px ${backgroundColor}"  @click=${() => this._command("Confirm")}>OK</button>
+                      <button class="btn ripple item_left" style="background-color: transparent;" @click=${() => this._command("CursorLeft")}><ha-icon icon="mdi:chevron-left"/></button>
+                      <button class="btn bnt_ok ripple item_ok" style="border: solid 2px ${backgroundColor}" @click=${() => this._command("Confirm")}>OK</button>
                       <button class="btn ripple item_right" style="background-color: transparent;" @click=${() => this._command("CursorRight")}><ha-icon icon="mdi:chevron-right"/></button>
-                      
                       <button class="btn ripple item_down" style="background-color: transparent;" @click=${() => this._command("CursorDown")}><ha-icon icon="mdi:chevron-down"/></button>
-                    </div>
+
+                      <button class="btn ripple item_ch_up" @click=${() => this._command("ChannelStepUp")}><ha-icon icon="mdi:chevron-up"/></button>
+                      <button class="btn item_ch_lbl" style="cursor: default;">P</button>
+                      <button class="btn ripple item_ch_down" @click=${() => this._command("ChannelStepDown")}><ha-icon icon="mdi:chevron-down"/></button>
+                  </div>
+
+                  <div class="grid-container-home">
+                      <button class="btn ripple" style="width: calc(var(--remotewidth)/6); height: calc(var(--remotewidth)/6);" @click=${() => this._command("Home")}><ha-icon icon="mdi:home"></ha-icon></button>
+                  </div>
 
                 ${colorButtons ? html`
                   <div class="grid-container-color_btn">
@@ -62,23 +70,9 @@ class philipsTvRemote extends LitElement {
                       <button class="btn-color ripple" style="background-color: blue; height: calc(var(--remotewidth) / 12);" @click=${e => this._command("BlueColour")}></button>
                   </div>
                   ` : ""}
-
-                  <div class="grid-container-volume-channel-control" >
-                      <button class="btn ripple"  style="border-radius: 50% 50% 0px 0px; margin: 0px auto 0px auto; height: 100%;" @click=${() => this._command("VolumeUp")}><ha-icon icon="mdi:plus"/></button>
-                      <button class="btn-flat flat-high ripple" style="margin-top: 0px; height: 50%;" @click=${() => this._command("Home")}><ha-icon icon="mdi:home"></button>
-                      <button class="btn ripple" style="border-radius: 50% 50% 0px 0px; margin: 0px auto 0px auto; height: 100%;" @click=${() => this._command("ChannelStepUp")}><ha-icon icon="mdi:chevron-up"/></button>
-                      
-                      <button class="btn ripple" style="border-radius: 0px; margin: 0px auto 0px auto; height: 100%; color:${stateObj.attributes.is_volume_muted === true ? 'red' : ''};" @click=${() => this._command("Mute")}><span class="${stateObj.attributes.is_volume_muted === true ? 'blink' : ''}"><ha-icon icon="${stateObj.attributes.is_volume_muted === true ? 'mdi:volume-off' : 'mdi:volume-mute'}"></span></button>
-                      <div></div>
-                      <button class="btn" style="border-radius: 0px; cursor: default; margin: 0px auto 0px auto; height: 100%;"><ha-icon icon="mdi:parking"/></button>
-                      
-                      <button class="btn ripple" style="border-radius: 0px 0px 50% 50%;  margin: 0px auto 0px auto; height: 100%;" @click=${() => this._command("VolumeDown")}><ha-icon icon="mdi:minus"/></button>
-                      <div></div>
-                      <button class="btn ripple" style="border-radius: 0px 0px 50% 50%;  margin: 0px auto 0px auto; height: 100%;"  @click=${() => this._command("ChannelStepDown")}><ha-icon icon="mdi:chevron-down"/></button>
-                  </div>
-                  </div>
               </div>
-            `;
+            </div>
+        `;
     }
 
     _command(command) {
@@ -109,9 +103,7 @@ class philipsTvRemote extends LitElement {
         return css`
         button:focus { outline:0; }
        .ripple {
-           position: relative;
-           overflow: hidden;
-           transform: translate3d(0, 0, 0);
+           position: relative; overflow: hidden; transform: translate3d(0, 0, 0);
       }
        .ripple:after {
            content: ""; display: block; position: absolute; border-radius: 50%; width: 100%; height: 100%; top: 0; left: 0; pointer-events: none;
@@ -121,52 +113,74 @@ class philipsTvRemote extends LitElement {
        .ripple:active:after { transform: scale(0, 0); opacity: .3; transition: 0s; }
        .blink { animation: blinker 1.5s linear infinite; color: red; }
        @keyframes blinker { 50% { opacity: 0; } }
+       
        .card { display: flex; justify-content: center; width: 100%; height: 100%; }
        .page {
            background-color: var(--remote-color); height: 100%; display: inline-block;
            border: var(--main-border-width) solid var(--main-border-color); border-radius: calc(var(--remotewidth) / 7.5);
            padding: calc(var(--remotewidth) / 37.5) calc(var(--remotewidth) / 15.2) calc(var(--remotewidth) / 25) calc(var(--remotewidth) / 15.2);
       }
-       .grid-container-power {
-           display: grid; grid-template-columns: 1fr 1fr 1fr; grid-template-rows: 1fr;
-           width: var(--remotewidth); height: calc(var(--remotewidth) / 4);
-      }
+      
+       /* 5x3 Grid für das gesamte Haupt-Layout */
        .grid-container-cursor {
-           display: grid; grid-template-columns: 1fr 1fr 1fr; grid-template-rows: 1fr 1fr 1fr;
-           height: var(--remotewidth); width: var(--remotewidth);
-           grid-template-areas: "menu up input" "left ok right" "back down exit";
+           display: grid;
+           grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+           grid-template-rows: 1fr 1fr 1fr;
+           width: var(--remotewidth);
+           height: calc(var(--remotewidth) * 0.65);
+           margin: auto;
+           grid-template-areas: 
+            "vol_up   source   up   power   ch_up"
+            "vol_mute left     ok   right   ch_lbl"
+            "vol_down options  down back    ch_down";
       }
-        .grid-container-color_btn{
-            display: grid; grid-template-columns: 1fr 1fr 1fr 1fr;
-            width: calc(var(--remotewidth) / 1.03); height: calc(var(--remotewidth) / 10); margin: 5px auto;
-        }
-        .grid-container-volume-channel-control {
-           display: grid; grid-template-columns: 1fr 1fr 1fr; grid-template-rows: 1fr 1fr 1fr;
-           width: var(--remotewidth); height: calc(var(--remotewidth) / 1.6);
-           margin-top: calc(var(--remotewidth) / -15); /* Zieht die Tasten nach oben */
-      }
-       .shape { grid-column: 1 / 4; grid-row: 1 / 4; padding: 5px; }
-       .item_menu { grid-area: menu; }
+      
+       /* Der Hintergrund füllt nun die mittleren 3 Spalten aus */
+       .shape { grid-column: 2 / 5; grid-row: 1 / 4; padding: 2px; }
+       
+       /* Lautstärke-Wippe Links */
+       .item_vol_up { grid-area: vol_up; border-radius: 50px 50px 0 0; width: 75%; height: 100%; margin: 0 auto; }
+       .item_vol_mute { grid-area: vol_mute; border-radius: 0; width: 75%; height: 100%; margin: 0 auto; }
+       .item_vol_down { grid-area: vol_down; border-radius: 0 0 50px 50px; width: 75%; height: 100%; margin: 0 auto; }
+       
+       /* Programm-Wippe Rechts */
+       .item_ch_up { grid-area: ch_up; border-radius: 50px 50px 0 0; width: 75%; height: 100%; margin: 0 auto; }
+       .item_ch_lbl { grid-area: ch_lbl; border-radius: 0; width: 75%; height: 100%; margin: 0 auto; font-weight: bold; font-size: calc(var(--remotewidth) / 15); }
+       .item_ch_down { grid-area: ch_down; border-radius: 0 0 50px 50px; width: 75%; height: 100%; margin: 0 auto; }
+
+       /* Ecken */
+       .item_source { grid-area: source; }
+       .item_power { grid-area: power; }
+       .item_options { grid-area: options; }
+       .item_back { grid-area: back; }
+
+       /* Steuerkreuz */
        .item_up { grid-area: up; }
-       .item_input { grid-area: input; }
-       .item_2_sx{ grid-area: left; }
-       .item_2_c { grid-area: ok; }
+       .item_left { grid-area: left; }
+       .item_ok { grid-area: ok; }
        .item_right { grid-area: right; }
        .item_down { grid-area: down; }
+
+       .grid-container-home {
+           display: flex; justify-content: center; margin-top: calc(var(--remotewidth) / 15);
+       }
+
+        .grid-container-color_btn{
+            display: grid; grid-template-columns: 1fr 1fr 1fr 1fr;
+            width: calc(var(--remotewidth) / 1.03); height: calc(var(--remotewidth) / 10); margin: 15px auto 5px auto;
+        }
+        
+       ha-icon { width: calc(var(--remotewidth) / 11); height: calc(var(--remotewidth) / 11); }
        
-       ha-icon { width: calc(var(--remotewidth) / 10.8); height: calc(var(--remotewidth) / 10.8); }
        .btn {
            background-color: var(--remote-button-color); color: var(--remote-text-color);
-           font-size: calc(var(--remotewidth) / 18.75); width: 70%; height: 70%;
+           width: 75%; height: 75%;
            border: 0; border-radius: 50%; margin: auto; cursor: pointer;
+           display: flex; justify-content: center; align-items: center;
       }
-       .btn-flat {
-           background-color: var(--remote-button-color); color: var(--remote-text-color);
-           border: 0; border-radius: calc(var(--remotewidth) / 10); margin: auto; cursor: pointer;
-      }
-       .flat-high { width: 70%; height: 45%; }
-       .bnt_ok { width: 100%; height:100%; font-size: calc(var(--remotewidth) / 16.6); }
+       .bnt_ok { width: 95%; height: 95%; font-size: calc(var(--remotewidth) / 16.6); }
        .title { display: block; text-align: center; font-weight: bold; padding-bottom: 5px; }
+       .btn-color { border-radius: calc(var(--remotewidth) / 10); border: 0; width: 70%; margin: auto; cursor: pointer; }
   `;
     }
 }
